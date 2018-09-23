923491c7c8e549f2c611a3916657c8a99be9458c
Aditya Suheryana <adityasuheryanaa@gmail.com>
2018-09-23 18:20:42 +0700

    first commit

---
+++ src/pbo3/pkg10117104/latihan7/integer/PBO310117104Latihan7Integer.java
@@ -0,0 +1,39 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pbo3.pkg10117104.latihan7.integer;
+
+/**
+ *
+ * @author  
+ * Nama      : Aditya Suheryana
+ * Kelas     : IF-3 / PBO-3
+ * Nim       : 10117104
+ * Deskripsi : Program ini menampilkan memory yang dig Tipe Data 
+ */
+public class PBO310117104Latihan7Integer {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+       byte b;
+       short s;
+       int i;
+       long l;
+       b = 120;
+       s = 32767;
+       i = 1_000_000_000;
+       l = 90000000000L;
+       
+        System.out.println("byte : " + b);
+        System.out.println("short : " + s);
+        System.out.println("int : " + i);
+        System.out.println("long : " + l);
+        
+    }
+    
+}
